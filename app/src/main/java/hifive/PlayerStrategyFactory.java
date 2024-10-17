2a95c0137e225e4517d9b650c88b79df723c00da
Viva La Vida <zxia312@outlook.com>
2024-10-17 21:57:24 +1100

    Strategy Factories.

--- app/src/main/java/hifive/PlayerStrategyFactory.java
+++ app/src/main/java/hifive/PlayerStrategyFactory.java
@@ -1,16 +1,21 @@
 package hifive;
 
 public class PlayerStrategyFactory {
-    public static PlayerStrategy createStrategy(String playerType, GameConfigurations config) {
-        switch(playerType.toLowerCase()) {
-            case "human":
-                return new HumanPlayerStrategy(config);
-            case "basic":
-                return new BasicPlayerStrategy();
-            case "clever":
-                return new CleverPlayerStrategy();
-            default:
-                return new RandomPlayerStrategy();
+    public static PlayerStrategy[] createPlayerStrategies(GameConfigurations config) {
+        PlayerStrategy[] strategies = new PlayerStrategy[config.NB_PLAYERS];
+        for (int i = 0; i < config.NB_PLAYERS; i++) {
+            String playerType = config.properties.getProperty("players." + i, "random").trim().toLowerCase();
+            strategies[i] = createStrategy(playerType, config);
         }
+        return strategies;
+    }
+
+    private static PlayerStrategy createStrategy(String playerType, GameConfigurations config) {
+        return switch(playerType.toLowerCase()) {
+            case "human" -> new HumanPlayerStrategy(config);
+            case "basic" -> new BasicPlayerStrategy();
+            case "clever" -> new CleverPlayerStrategy();
+            default -> new RandomPlayerStrategy();
+        };
     }
 }
